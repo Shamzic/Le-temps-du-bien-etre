4fb66e07021180a9a2c1c768bff8cfc8a11d68eb
Shamzic <shamery@free.fr>
2020-05-24 20:01:45 +0200

    [indexPage] update

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
 
-import Layout from '../components/Layout'
-import Features2 from '../components/Features2'
-import BlogRoll from '../components/BlogRoll'
+import Layout from "../components/Layout";
+import Features2 from "../components/Features2";
+import BlogRoll from "../components/BlogRoll";
 
-import './index-page-style.css'
+import "./index-page-style.css";
+import { Helmet } from "react-helmet";
 
 export const IndexPageTemplate = ({
   image,
@@ -18,8 +19,15 @@ export const IndexPageTemplate = ({
   intro,
 }) => (
   <div>
+    <Helmet>
+      <meta charSet='utf-8' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Cinzel+Decorative&family=Open+Sans&display=swap'
+        rel='stylesheet'
+      />
+    </Helmet>
     <div
-      className="full-width-image margin-top-0"
+      className='full-width-image margin-top-0'
       style={{
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
@@ -28,83 +36,105 @@ export const IndexPageTemplate = ({
     >
       <div
         style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-          flexDirection: 'column',
-          fontFamily: 'Montserrat, sans-serif'
+          display: "flex",
+          height: "150px",
+          lineHeight: "1",
+          justifyContent: "space-around",
+          alignItems: "center",
+          flexDirection: "column",
+          fontFamily: "Cinzel Decorative, cursive",
         }}
       >
         <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+          className='has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen'
           style={{
             boxShadow:
-              'rgb(11, 19, 3, 0.6) 0.5rem 0px 0px, rgb(11, 19, 3, 0.6) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(11, 19, 3, 0.6)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-            borderRadius: '15px'
+              "rgb(11, 19, 3, 0.6) 0.5rem 0px 0px, rgb(11, 19, 3, 0.6) -0.5rem 0px 0px",
+            backgroundColor: "rgb(11, 19, 3, 0.6)",
+            color: "white",
+            lineHeight: "1",
+            padding: "0.25em",
+            borderRadius: "15px",
           }}
         >
           {title}
         </h1>
         <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+          className='has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen'
           style={{
             boxShadow:
-              'rgb(11, 19, 3, 0.6) 0.5rem 0px 0px, rgb(11, 19, 3, 0.6) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(11, 19, 3, 0.6)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-            textAlign: 'center',
-            borderRadius: '15px'
+              "rgb(11, 19, 3, 0.6) 0.5rem 0px 0px, rgb(11, 19, 3, 0.6) -0.5rem 0px 0px",
+            backgroundColor: "rgb(11, 19, 3, 0.6)",
+            color: "white",
+            lineHeight: "1",
+            padding: "0.25em",
+            textAlign: "center",
+            borderRadius: "15px",
           }}
         >
           {subheading}
         </h3>
       </div>
     </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile" >
-                    <h3 className="title">{mainpitch.title}</h3>
+    <section className='section section--gradient'>
+      <div className='container'>
+        <div className='section'>
+          <div className='columns'>
+            <div className='column is-10 is-offset-1'>
+              <div className='content'>
+                <div className='content'>
+                  <div
+                    className='tile'
+                    style={{
+                      fontFamily: "Open Sans, sans-serif",
+                    }}
+                  >
+                    <h4 className='title'>{mainpitch.title}</h4>
                   </div>
-                  <div className="tile">
-                    <p className="subtitle" style={{ fontStyle: 'italic'}}>{mainpitch.description}</p>
+                  <div className='tile'>
+                    <p
+                      className='subtitle'
+                      style={{
+                        fontStyle: "italic",
+                        marginBottom: "5%",
+                        fontSize: "17px",
+                      }}
+                    >
+                      {mainpitch.description}
+                    </p>
                   </div>
                 </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2"  style={{textAlign: 'center'}}>
+                <div className='columns'>
+                  <div className='column is-12'>
+                    <h4
+                      className='has-text is-size-3'
+                      style={{
+                        textAlign: "center",
+                        fontFamily: "Open Sans, sans-serif",
+                      }}
+                    >
                       {heading}
-                    </h3>
-                    <p >{description} </p>
+                    </h4>
+                    <p style={{ fontFamily: "Open Sans, sans-serif" }}>
+                      {description}{" "}
+                    </p>
                   </div>
                 </div>
                 <Features2 gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
+                <div className='columns'>
+                  <div className='column is-12 has-text-centered'>
+                    <Link className='btn' to='/products'>
                       Voir toutes les prestations
                     </Link>
                   </div>
                 </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
+                <div className='column is-12'>
+                  <h3 className='has-text-weight-semibold is-size-2'>
                     Dernières actualités
                   </h3>
                   <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
+                  <div className='column is-12 has-text-centered'>
+                    <Link className='btn' to='/blog'>
                       Lire plus
                     </Link>
                   </div>
@@ -116,7 +146,7 @@ export const IndexPageTemplate = ({
       </div>
     </section>
   </div>
-)
+);
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -128,10 +158,10 @@ IndexPageTemplate.propTypes = {
   intro: PropTypes.shape({
     blurbs: PropTypes.array,
   }),
-}
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
@@ -145,8 +175,8 @@ const IndexPage = ({ data }) => {
         intro={frontmatter.intro}
       />
     </Layout>
-  )
-}
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
@@ -154,9 +184,9 @@ IndexPage.propTypes = {
       frontmatter: PropTypes.object,
     }),
   }),
-}
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
@@ -194,4 +224,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
